b21d6110ef50a9545099c0a276db6bf1a92a7574
David Teixeira <davidpedreira250@gmail.com>
2026-05-21 13:50:45 -0300

    Add files via upload

---
+++ Graphical User Interface/CalculadoraJFrame/src/main/java/com/mycompany/calculadorajframe/CalculadoraJFrame.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadorajframe;
+
+/**
+ *
+ * @author Admin
+ */
+public class CalculadoraJFrame {
+
+    public static void main(String[] args) {
+        JanelaPrincipal app1 = new JanelaPrincipal();
+        
+    }
+}
